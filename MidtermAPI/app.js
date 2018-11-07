4af0cf6ec984cf8ca090763176cb3638ab02154d
aminie45 <aminie45@gmail.com>
2018-11-07 18:49:40 -0500

    change JS structure

--- MidtermAPI/app.js
+++ MidtermAPI/app.js
@@ -1,135 +1,36 @@
-const hero = document.getElementById('hero');
-const gifs = document.getElementById('gifs');
+const endpoint = "https://api.giphy.com/v1/gifs/random";
+const key = "51SLNTvlfvAfSzYAcfdq7Tlg9qI6SoRB";
 
-hero.addEventListener("keydown", function (event) {
-    if (event.which == 13) {
+const fav = document.getElementsByClassName('fav');
+for (let i = 0; i < fav.length; i++) {
+    fav[i].addEventListener('keydown', function (event) {
+        if (event.which == 13) {
+            addGif(event.currentTarget.value, event.currentTarget.id);
+        }
+    })
+}
 
-        // get search term
-        const h = hero.value;
-        //        const = shoe.value;
+// random url
+function addGif(tag, id) {
+    const url = `${endpoint}?tag=${tag}&api_key=${key}&rating=g`;
 
-        // create search url
 
-        // search endpoint
-        //		const endpoint = "https://api.giphy.com/v1/gifs/search";
-        // random endpoint 
-        const endpoint = "https://api.giphy.com/v1/gifs/random";
-        const key = "51SLNTvlfvAfSzYAcfdq7Tlg9qI6SoRB";
-
-        // search url
-        //		const url = `${endpoint}?q=${q}&api_key=${key}`;
-        // random url
-        const url = `${endpoint}?tag=${h}&api_key=${key}&rating=g`;
-        //        const url = `${endpoint}?tag=${s}&api_key=${key}&rating=g`;
-
-        const promise = fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-        promise.then(function (json) {
-            // for loop for search results
-            //			for (let i = 0; i < json.data.length; i++) {
-            //				const gif = json.data[i];
-
-            // one gif for random results
-            const gif = json.data;
-
-            // add image to div
-            const image = new Image();
-            image.src = gif.images.downsized.url;
-            gifs.appendChild(image);
-            //			}
+    const promise = fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-    }
-});
 
-const shoe = document.getElementById('shoe');
-const gifs2 = document.getElementById('gifs2');
+    promise.then(function (json) {
 
-shoe.addEventListener("keydown", function (event) {
-    if (event.which == 13) {
+        // one gif for random results
+        const gif = json.data;
 
-        // get search term
-        const s = shoe.value;
-        //        const = shoe.value;
-
-        // create search url
-
-        // search endpoint
-        //		const endpoint = "https://api.giphy.com/v1/gifs/search";
-        // random endpoint 
-        const endpoint = "https://api.giphy.com/v1/gifs/random";
-        const key = "51SLNTvlfvAfSzYAcfdq7Tlg9qI6SoRB";
-
-        // search url
-        //		const url = `${endpoint}?q=${q}&api_key=${key}`;
-        // random url
-        const url = `${endpoint}?tag=${s}&api_key=${key}&rating=g`;
-        //        const url = `${endpoint}?tag=${s}&api_key=${key}&rating=g`;
-
-        const promise = fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-        promise.then(function (json) {
-            // for loop for search results
-            //			for (let i = 0; i < json.data.length; i++) {
-            //				const gif = json.data[i];
-
-            // one gif for random results
-            const gif = json.data;
-
-            // add image to div
-            const image = new Image();
-            image.src = gif.images.downsized.url;
-            gifs2.appendChild(image);
-            //			}
-        });
-    }
-});
-
-const sports = document.getElementById('sports');
-const gifs3 = document.getElementById('gifs3');
-
-sports.addEventListener("keydown", function (event) {
-    if (event.which == 13) {
-
-        // get search term
-        const sp = sports.value;
-
-        // random endpoint 
-        const endpoint = "https://api.giphy.com/v1/gifs/random";
-        const key = "51SLNTvlfvAfSzYAcfdq7Tlg9qI6SoRB";
-
-        // random url
-        const url = `${endpoint}?tag=${sp}&api_key=${key}&rating=g`;
-
-        const promise = fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-        promise.then(function (json) {
-
-            // one gif for random results
-            const gif = json.data;
-
-            // add image to div
-            const image = new Image();
-            image.src = gif.images.downsized.url;
-            gifs3.appendChild(image);
-
-        });
-    }
-});
+        // add image to div
+        const image = new Image();
+        image.src = gif.images.downsized.url;
+        document.getElementById(id + "-gifs").appendChild(image);
+    });
+}
